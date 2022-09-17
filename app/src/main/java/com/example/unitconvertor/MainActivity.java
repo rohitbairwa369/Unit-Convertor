1b77d9f748121207e3a3984079edd9aee24a4b5e
unknown <rohitbairwa36910@gmail.com>
2022-09-18 02:32:38 +0530

    firt commit

---
+++ app/src/main/java/com/example/unitconvertor/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.unitconvertor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+    private EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button =findViewById(R.id.button);
+        textView =findViewById(R.id.textView);
+        editText =findViewById(R.id.editTextNumber);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Om Namaha Shivaya", Toast.LENGTH_SHORT).show();
+                String s =editText.getText().toString();
+                int kg=Integer.parseInt(s);
+                double pound =2.205*kg;
+                textView.setText("The Corresponding value in pound is : "+pound);
+
+
+            }
+        });
+
+    }
+}
